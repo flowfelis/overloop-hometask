e1c0d234d93286c1d63a06366c590c24ef50e9a6
ali <alicandonmez90@gmail.com>
2021-09-30 15:53:02 +0200

    create authors django-app

--- techtest/settings.py
+++ techtest/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'techtest.articles',
-    'techtest.regions'
+    'techtest.regions',
+    'techtest.authors',
 ]
 
 MIDDLEWARE = [
