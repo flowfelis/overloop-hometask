416ef1f0c34ea3029628a9e5772204cf5abd800d
ali <alicandonmez90@gmail.com>
2021-10-01 20:20:49 +0200

    update for the "mutable parameter" issue

--- techtest/utils.py
+++ techtest/utils.py
@@ -2,7 +2,10 @@ import json
 from django.http.response import HttpResponse
 
 
-def json_response(data={}, status=200):
+def json_response(data=None, status=200):
+    # https://florimond.dev/en/posts/2018/08/python-mutable-defaults-are-the-source-of-all-evil/
+    if data is None:
+        data = {}
     return HttpResponse(
         content=json.dumps(data), status=status, content_type="application/json"
     )
